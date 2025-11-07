10dbbe5a02ac25159614a2966b76e580014df8b8
soriphoono <soriphoono@gmail.com>
2025-11-07 17:11:10 -0500

    Updated docker compose files

--- init.sh
+++ init.sh
@@ -1,7 +1,1 @@
 #!/usr/bin/env bash
-
-docker swarm init --advertise-addr 192.168.0.21
-
-docker network create -d overlay traefik_public
-
-docker stack deploy server -c ./server/docker-compose.yml
