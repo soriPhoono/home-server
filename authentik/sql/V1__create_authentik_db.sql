97104f2fa5e43884d0f35492216ba117317a0724
soriphoono <soriphoono@gmail.com>
2025-11-12 10:10:20 -0600

    Organization of code blocks, migrated from flyway to standalone start script running on the alpine image of postgres for dynamic database provisioning

--- authentik/sql/V1__create_authentik_db.sql
+++
@@ -1,6 +0,0 @@
--- Create authentik database
-CREATE DATABASE IF NOT EXISTS authentik;
-
--- Create authentik database user with password from environment variable AUTHENTIK_DB_PASSWORD
-CREATE USER IF NOT EXISTS 'authentik'@'%' IDENTIFIED BY '${AUTHENTIK_DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON authentik.* TO 'authentik'@'%';
