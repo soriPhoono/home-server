7510df86f386fb6293ef398086dd5412ef0ae4bd
soriphoono <soriphoono@gmail.com>
2025-11-19 19:30:02 -0600

    Updated script with assistant launcher of test env

--- flake.nix
+++ flake.nix
@@ -16,13 +16,35 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells.default =
-        pkgs.mkShell {
-          packages = with pkgs; [
-            k3d
-            kubectl
-            helm
-          ];
-        };
+      packages.spawnTestEnv = pkgs.writeShellApplication {
+        name = "spawn-test-env.sh";
+
+        runtimeInputs = with pkgs; [
+          k3d
+          kubectl
+          helm
+        ];
+
+        text = ''
+          set -euo pipefail
+
+          CLUSTER_NAME=$1
+
+          k3d cluster create "$CLUSTER_NAME" \
+              --servers 3 \
+              --agents 6 \
+              --volume "/dev/mapper/crypted:/dev/mapper/crypted@all" \
+              --volume "/sys/fs/cgroup:/sys/fs/cgroup@all" \
+              --k3s-arg "--kubelet-arg=fail-swap-on=false@all"
+        '';
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          k3d
+          kubectl
+          helm
+        ];
+      };
     });
 }
